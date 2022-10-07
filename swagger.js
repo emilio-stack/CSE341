0fb7a945bf39cfd9202905afade43544f72130a9
emilio-stack <84351829+emilio-stack@users.noreply.github.com>
2022-10-07 11:38:39 -0600

    regenerating the api docs

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'Contacts API',
     description: 'A REST API for contacts from my mission',
   },
-  host: 'localhost:8080',
-  schemes: ['http'],
+  host: 'https://personal-activity.onrender.com',
+  schemes: ['https'],
 };
 
 const outputFile = './swagger-output.json';
